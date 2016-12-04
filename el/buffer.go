e10310df5b211011723e81dc5bf06f768ed7a424
Mahmoud Abdelsalam <scroveez@gmail.com>
2016-12-04 10:46:57 +0300

    fixing headers

--- el/buffer.go
+++ el/buffer.go
@@ -12,7 +12,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
-* Author: Justin Wong <justin.w.xd@gmail.com>
+* Author: Mahmoud Abdelsalam <scroveez@gmail.com>
 *
  */
 
